e9f437042e6cb6c4a4652ea642771aafa3605ea4
Benjamin-Botten <benjobot@gmail.com>
2015-11-22 21:25:00 +0100

    Gave BitOperatorTest-cases more conventionally correct declarations. (Since the operatorions are functional interfaces, I think it's reasonable to just call them by their names.)

--- TDD Assignment/src/test/BitOperatorTest.java
+++ TDD Assignment/src/test/BitOperatorTest.java
@@ -12,14 +12,14 @@ import static øving.Utility.*;
 public class BitOperatorTest {
 
     @Test
-    public void AND_applyTo_BitStringContaining000000000000000011000011AndBitStringContaining000000000000000001001110_ShouldReturn000000000000000001000010() {
+    public void AND_BitStringContaining000000000000000011000011AndBitStringContaining000000000000000001001110_ShouldReturn000000000000000001000010() {
         BitString firstString = new BitString("000000000000000011000011");
         BitString secondString = new BitString("000000000000000001001110");
         assertThat(BitOperator.AND.applyTo(firstString, secondString), is(equalTo("000000000000000001000010")));
     }
     
     @Test
-    public void OR_applyTo_BitStringContaining000000000000000011000011AndBitStringContaining000000000000000001001110_ShouldReturn000000000000000011001111() {
+    public void OR_BitStringContaining000000000000000011000011AndBitStringContaining000000000000000001001110_ShouldReturn000000000000000011001111() {
         BitString firstString = new BitString("000000000000000011000011");
         BitString secondString = new BitString("000000000000000001001110");
         assertThat(BitOperator.OR.applyTo(firstString, secondString), is(equalTo("000000000000000011001111")));
